84aebc716f791ea932c0c6e7fd6113d59ad74add
Veto Ramirez <vetorobert3@gmail.com>
2022-06-12 09:58:59 -0700

    pushing so that other computer can pull changes. Put in args and both a put and get request. Having problems with the linux machine. Seeing if it will work on the windows.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 api = Api(app)
 
 quote_put_args = reqparse.RequestParser()
-quote_put_args.add_argument("author", type=str, help="name of author")
-quote_put_args.add_argument("quote", type=str, help="quote of the author")
+quote_put_args.add_argument("author", type=str, help="name of author", required=True)
+quote_put_args.add_argument("quote", type=str, help="quote of the author", required=True)
 
 quotes = {}
 
@@ -14,8 +14,10 @@ class Quote(Resource):
     def get(self, quote_id):
         return quotes[quote_id]
 
-    def put(self, quote_id):    
-        return
+    def put(self, quote_id):
+        args = quote_put_args.parse_args()
+        quotes[quote_id] = args
+        return quotes[quote_id], 201
 
 api.add_resource(Quote, "/quote/<int:quote_id>")
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -2,5 +2,10 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld/sara", {"likes": 10})
+response = requests.put(BASE + "quote/1", {"author": "Buddha", "quote": "live and let die"})
+print(response.json())
+
+input()
+
+response = requests.get(BASE + "quote/1")
 print(response.json())
