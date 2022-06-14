6609d806b9b7fab31a15c3d94cf43c1edc03be7b
Veto Ramirez <vetorobert3@gmail.com>
2022-06-13 22:25:52 -0700

    Added a delete method for our API. Added multiple id's of differnt quotes, deleted one and was able do a get request of a quote with a particular id.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,14 +10,29 @@ quote_put_args.add_argument("quote", type=str, help="quote of the author", requi
 
 quotes = {}
 
+def abort_if_quote_id_doesnt_exist(quote_id):
+    if quote_id not in quotes:
+        abort(404, message="Could not find quote...")
+
+def abort_if_quote_exists(quote_id):
+    if quote_id in quotes:
+        abort(409, message="Quote already exists with that id...")
+
 class Quote(Resource):
     def get(self, quote_id):
+        abort_if_quote_id_doesnt_exist(quote_id)
         return quotes[quote_id]
 
     def put(self, quote_id):
+        abort_if_quote_exists(quote_id)
         args = quote_put_args.parse_args()
         quotes[quote_id] = args
         return quotes[quote_id], 201
+
+    def delete(self, quote_id):
+        abort_if_quote_id_doesnt_exist(quote_id)
+        del quotes[quote_id]
+        return "", 204
 
 api.add_resource(Quote, "/quote/<int:quote_id>")
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -2,10 +2,23 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "quote/1", {"author": "Buddha", "quote": "live and let die"})
-print(response.json())
+data = [
+    {"author": "Thomas Carlyle", "quote": "No pressure, no diamonds."},
+    {"author": "Helen Keller", "quote": "We can do anything we want if we stick to it long enough."},
+    {"author": "Samuel Beckett", "quote": "Try again. Fail again. Fail better"},
+    {"author": "Seneca", "quote": "He who is brave is free"}
+]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "quote/" + str(i), data[i])
+    print(response.json())
 
 input()
 
-response = requests.get(BASE + "quote/1")
+response = requests.delete(BASE + "quote/0")
+print(response)
+
+input()
+
+response = requests.get(BASE + "quote/2")
 print(response.json())
