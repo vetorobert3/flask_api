ffea45a65d7f3a326093fb6ab8b5b505540ab4ea
Veto Ramirez <vetorobert3@gmail.com>
2023-01-20 12:07:00 -0800

    add instructins for running on a venv, code in the app page to running on browser, and that is it I think.

--- app_page.py
+++ app_page.py
@@ -1,10 +1,14 @@
-from flask import Flask
-
-app = Flask(__name__)
+from flask import current_app as app
+from flask import render_template 
 
 @app.route("/")
 def home():
-  return "running..."
+  """Landing Page"""
+  return render_template(
+    'home.html',
+    title="Jinja Demo Site"
+    description="Smarter page templates with Flask & Jinja."
+  )
 
 if __name__ == '__main__':
   app.run(debug=True)
