d57a379ab09def80f7a1cee907974808a5bc0018
Veto Ramirez <vetorobert3@gmail.com>
2023-02-06 20:47:41 -0800

    Got sign up and login frontends up. Working on the GET anf POST requests for both pages.

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
-  return "<p>Login</p>"
+  return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
   return "<p>Logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
-  return "<p>Sign Up</p>"
+  if request.method == 'POST':
+    email = request.form.get('email')
+    firstName = request.form.get('firstName')
+    password1 = request.form.get('password1')
+    password2 = request.form.get('password2')
+
+    if len(email) < 4:
+      pass
+    elif len(firstName) <2:
+      pass
+    elif password1 != password2:
+      pass
+    elif len(password1) <7:
+      pass
+    else:
+      # add user to database
+      pass
+
+  return render_template("sign_up.html")
